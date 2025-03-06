dc23ae5f8aad722ef434ce8fc5b41379c301ea50
Arnav Saha <sahaarnav3@gmail.com>
2025-03-06 18:31:41 +0530

    Added all the routes and updated README with all the routes data

--- README.md
+++ README.md
@@ -3,7 +3,12 @@
 
 | Purpose      | METHOD          | Endpoint  |
 | ------------- |:-------------:| -----:|
+|   Route to fetch HomePage.   | `GET` | / |
 |   Route to save a new recipe.   | `POST` | /recipes |
 |   Route to fetch all recipes from the DataBase.   | `GET` | /recipes |
 |   Route to fetch recipe of given title from DataBase.   | `GET` | /recipes/title/:titleName |
 |   Route to fetch recipe of a given author from DataBase.   | `GET` | /recipes/author/:authorName |
+|   Route to fetch all "Easy" recipes from DataBase.   | `GET` | /recipes/difficulty-easy |
+|   Route to update the difficulty level of a recipe with it's ID.   | `POST` | /recipes/update-difficulty/:recipeId |
+|   Route to update the prep and cook timings of a recipe with it's title.   | `POST` | /recipes/update-timings/:recipeTitle |
+|   Route to delete a recipe with its ID.   | `DELETE` | /recipes/:recipeID |
--- index.js
+++ index.js
@@ -21,26 +21,20 @@ app.post("/recipes", async (req, res) => {
     const recipe = new Recipe(req.body);
     const saveRecipe = await recipe.save();
     if (!saveRecipe)
-      res
-        .status(404)
-        .json({
-          error:
-            "Error occurred while saving new Recipe. Please check data format and try again.",
-        });
-    else
-      res
-        .status(200)
-        .json({
-          message: "New Recipe saved successfully.",
-          "Recipe-Data": saveRecipe,
-        });
-  } catch (error) {
-    res
-      .status(500)
-      .json({
+      res.status(404).json({
         error:
-          "Some error occurred with the request to add recipe to DB. Please try again.",
+          "Error occurred while saving new Recipe. Please check data format and try again.",
       });
+    else
+      res.status(200).json({
+        message: "New Recipe saved successfully.",
+        "Recipe-Data": saveRecipe,
+      });
+  } catch (error) {
+    res.status(500).json({
+      error:
+        "Some error occurred with the request to add recipe to DB. Please try again.",
+    });
   }
 });
 
@@ -49,17 +43,38 @@ app.get("/recipes", async (req, res) => {
   try {
     const recipeData = await Recipe.find();
     if (!recipeData)
+      res.status(404).json({
+        error:
+          "Either some error occured or there is no entry present in the DB.",
+      });
+    else
+      res.status(200).json({
+        message: "All Recipes fetched successfully.",
+        "Recipe-Data": recipeData,
+      });
+  } catch (error) {
+    res.status(500).json({
+      error: "Some error occured with the request itself. Please try again.",
+    });
+  }
+});
+
+//Route to fetch Recipe Data by it's title.
+app.get("/recipes/title/:title", async (req, res) => {
+  try {
+    const recipeData = await Recipe.findOne({ title: req.params.title });
+    if (!recipeData)
       res
         .status(404)
         .json({
           error:
-            "Either some error occured or there is no entry present in the DB.",
+            "No recipe exists with the given title. Please try again with proper data.",
         });
     else
       res
         .status(200)
         .json({
-          message: "All Recipes fetched successfully.",
+          message: "Recipe with given title fetched successfully.",
           "Recipe-Data": recipeData,
         });
   } catch (error) {
@@ -71,44 +86,121 @@ app.get("/recipes", async (req, res) => {
   }
 });
 
-//Route to fetch Recipe Data by it's title.
-app.get("/recipes/title/:title", async (req, res) => {
-  try {
-    const recipeData = await Recipe.findOne({ title: req.params.title});
-    if(!recipeData)
-        res.status(404).json({ error: "No recipe exists with the given title. Please try again with proper data."});
-    else
-        res.status(200).json({ message: "Recipe with given title fetched successfully.", "Recipe-Data": recipeData });
-  } catch (error) {
-    res.status(500).json({ error: "Some error occured with the request itself. Please try again." });
-  }
-});
-
 //Route to fetch Recipe Data by it's author.
 app.get("/recipes/author/:authorName", async (req, res) => {
   try {
-    const recipeData = await Recipe.findOne({ author: req.params.authorName});
-    if(!recipeData)
-        res.status(404).json({ error: "No recipe exists with the given Author. Please try again with proper data."});
+    const recipeData = await Recipe.findOne({ author: req.params.authorName });
+    if (!recipeData)
+      res
+        .status(404)
+        .json({
+          error:
+            "No recipe exists with the given Author. Please try again with proper data.",
+        });
     else
-        res.status(200).json({ message: "Recipe with given Author fetched successfully.", "Recipe-Data": recipeData });
+      res
+        .status(200)
+        .json({
+          message: "Recipe with given Author fetched successfully.",
+          "Recipe-Data": recipeData,
+        });
   } catch (error) {
-    res.status(500).json({ error: "Some error occured with the request itself. Please try again." });
+    res
+      .status(500)
+      .json({
+        error: "Some error occured with the request itself. Please try again.",
+      });
   }
 });
 
 //Route to fetch all Recipe Data what have "Easy" difficulty.
 app.get("/recipes/difficulty-easy", async (req, res) => {
   try {
-    const recipeData = await Recipe.find({ difficulty: "Easy"});
-    if(!recipeData)
-        res.status(404).json({ error: "No recipe exists with the given difficulty. Please try again with proper data."});
+    const recipeData = await Recipe.find({ difficulty: "Easy" });
+    if (!recipeData)
+      res
+        .status(404)
+        .json({
+          error:
+            "No recipe exists with the given difficulty. Please try again with proper data.",
+        });
     else
-        res.status(200).json({ message: "Recipe with given difficulty fetched successfully.", "Recipe-Data": recipeData });
+      res
+        .status(200)
+        .json({
+          message: "Recipe with given difficulty fetched successfully.",
+          "Recipe-Data": recipeData,
+        });
   } catch (error) {
-    res.status(500).json({ error: "Some error occured with the request itself. Please try again." });
+    res
+      .status(500)
+      .json({
+        error: "Some error occured with the request itself. Please try again.",
+      });
   }
 });
+
+//Route to update the difficulty level of a recipe with it's ID
+app.post("/recipes/update-difficulty/:recipeId", async (req, res) => {
+  try {
+    const recipeData = await Recipe.findByIdAndUpdate(
+      req.params.recipeId,
+      { difficulty: "Easy" },
+      { new: true }
+    );
+    if(!recipeData)
+        res.status(404).json({ error: "Either the recipe with given ID doesn't exist. Or some data error. Please try again with proper data."})
+    else
+        res.status(200).json({ message: "Data updated successfully.", "New-Recipe-Data": recipeData});
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({
+        error: "Some error occured with the request itself. Please try again.",
+      });
+  }
+});
+
+//Route to update the prep time and cook time of a recipe with it's title.
+app.post("/recipes/update-timings/:recipeTitle", async (req, res) => {
+  try {
+    const recipeData = await Recipe.findOneAndUpdate(
+      { title: req.params.recipeTitle },
+      { prepTime: 40, cookTime: 45 },
+      { new: true }
+    );
+    if(!recipeData)
+        res.status(404).json({ error: "Either the recipe with given ID doesn't exist. Or some data error. Please try again with proper data."})
+    else
+        res.status(200).json({ message: "Data updated successfully.", "New-Recipe-Data": recipeData});
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({
+        error: "Some error occured with the request itself. Please try again.",
+      });
+  }
+});
+
+//Route to update the difficulty level of a recipe with it's ID
+app.delete("/recipes/:recipeId", async (req, res) => {
+    try {
+      const recipeData = await Recipe.findByIdAndDelete(req.params.recipeId);
+      if(!recipeData)
+          res.status(404).json({ error: "Either the recipe with given ID doesn't exist. Or some data error. Please try again with proper data."})
+      else
+          res.status(200).json({ message: "Recipe Deleted Successfully." });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({
+          error: "Some error occured with the request itself. Please try again.",
+        });
+    }
+  });
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT:", PORT);
